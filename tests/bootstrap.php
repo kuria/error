bf40d9fcaf30369c8456a9172244edba2e2fd337
ShiraNai7 <shiranai7@hotmail.com>
2017-05-08 02:14:22 +0200

    Require PHPUnit as a dev dependency

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,4 @@
 
 mb_internal_encoding('UTF-8');
 
-if (is_file(__DIR__ . '/../vendor/autoload.php')) {
-    $classLoader = require __DIR__ . '/../vendor/autoload.php';
-}
+require __DIR__ . '/../vendor/autoload.php';
